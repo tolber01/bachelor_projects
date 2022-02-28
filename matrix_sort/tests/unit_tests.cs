03d7dc2a5a16f95f742d4ab67bdc57ccaeab8b8f
tolber01 <ov4.t@yandex.ru>
2022-02-28 13:33:05 +0300

    Added unit tests for all matrix sorts types

--- matrix_sort/tests/unit_tests.cs
+++ matrix_sort/tests/unit_tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Linq;
 
 namespace tests
 {
@@ -6,6 +7,27 @@ namespace tests
     {
         private int[,] testMatrix;
         private matrix_sort.MatrixSorter sorter;
+
+        private int[][] toJagged(int[,] array2d)
+        {
+            int[][] result = new int[array2d.GetLength(0)][];
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                result[i] = new int[array2d.GetLength(1)];
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                    result[i][j] = array2d[i, j];
+            }
+            return result;
+        }
+
+        private int[,] to2d(int[][] jagged)
+        {
+            int[,] result = new int[jagged.Length, jagged[0].Length];
+            for (int i = 0; i < result.GetLength(0); i++)
+                for (int j = 0; j < result.GetLength(1); j++)
+                    result[i, j] = jagged[i][j];
+            return result;
+        }
 
         [SetUp]
         public void Setup()
@@ -24,21 +46,50 @@ namespace tests
         {
             sorter.IsLessStrategy = matrix_sort.SortOrders.SumIsLess;
 
+            int[][] jagged;
             sorter.SortMatrix(testMatrix, ascending: true);
-            Assert.AreEqual(testMatrix, new int[,] {
-                { 1, 2, 3, -4, -5, -6 },
-                { 12, -5, -8, -9, 3, 5 },
-                { -1, 2, -5, 6, 7, 0 },
-                { 0, 4, 6, 8, -1, -3 },
-            });
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => row.Sum()).ToArray()));
 
             sorter.SortMatrix(testMatrix, ascending: false);
-            Assert.AreEqual(testMatrix, new int[,] {
-                { 0, 4, 6, 8, -1, -3 },
-                { -1, 2, -5, 6, 7, 0 },
-                { 12, -5, -8, -9, 3, 5 },
-                { 1, 2, 3, -4, -5, -6 },
-            });
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => -row.Sum()).ToArray()));
+        }
+
+        [Test]
+        public void MaxSortTest()
+        {
+            sorter.IsLessStrategy = matrix_sort.SortOrders.MaxIsLess;
+
+            int[][] jagged;
+            sorter.SortMatrix(testMatrix, ascending: true);
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => row.Max()).ToArray()));
+
+            sorter.SortMatrix(testMatrix, ascending: false);
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => -row.Max()).ToArray()));
+        }
+
+        [Test]
+        public void MinSortTest()
+        {
+            sorter.IsLessStrategy = matrix_sort.SortOrders.MinIsLess;
+
+            int[][] jagged;
+            sorter.SortMatrix(testMatrix, ascending: true);
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => row.Min()).ToArray()));
+
+            sorter.SortMatrix(testMatrix, ascending: false);
+
+            jagged = toJagged(testMatrix);
+            Assert.AreEqual(testMatrix, to2d(jagged.OrderBy(row => -row.Min()).ToArray()));
         }
     }
 }
