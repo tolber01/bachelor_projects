a9d4aae0c5a5e5e259e70e6c1accbcc8433dd7d9
tolber01 <ov4.t@yandex.ru>
2022-02-28 17:59:53 +0300

    Added basic user interface in matrix_sort

--- matrix_sort/matrix_sort/Program.cs
+++ matrix_sort/matrix_sort/Program.cs
@@ -4,6 +4,17 @@ namespace matrix_sort
 {
     class Program
     {
+        private enum CharCodes
+        {
+            BySum = '1',
+            ByMax = '2',
+            ByMin = '3',
+            Asc = '1',
+            Desc = '2'
+        }
+        private static string ORDER_TYPE_MSG = $"Choose rows sorting type:\r\n{(char)CharCodes.BySum} - by sum\r\n{(char)CharCodes.ByMax} - by max\r\n{(char)CharCodes.ByMin} - by min";
+        private static string SORT_TYPE_MSG = $"Choose type of sorting:\r\n{(char)CharCodes.Asc} - ascending\r\n{(char)CharCodes.Desc} - descending";
+
         static void PrintMatrix(int[,] matrix)
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
@@ -19,25 +30,47 @@ namespace matrix_sort
         {
             var sorter = new MatrixSorter();
             var matrix = new int[,] {
-                { 11, -14, 5, 6 }, // sum = 8, max = 11, min = -14
-                { 1, 3, -2, -1 },  // sum = 1, max = 3, min = -2
-                { 0, -3, -5, 3 }, // sum = -4, max = 3, min = -5
-                { 1, -1, 2, -2 } // sum = 0, max = 2, min = -2
+                { 11, -14,  5,  6 }, // sum = 8, max = 11, min = -14
+                {  1,   3, -2, -1 }, // sum = 1, max = 3, min = -2
+                {  0,  -3, -5,  3 }, // sum = -4, max = 3, min = -5
+                {  1,  -1,  2, -2 }  // sum = 0, max = 2, min = -2
             };
 
-            Console.WriteLine("Sorted rows by sum (ascending):");
-            sorter.IsLessStrategy = SortOrders.SumIsLess;
-            sorter.SortMatrix(matrix);
-            PrintMatrix(matrix);
+            char orderType;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(ORDER_TYPE_MSG);
+                orderType = Console.ReadKey(true).KeyChar;
+            } while (orderType != (char)CharCodes.BySum
+                  && orderType != (char)CharCodes.ByMax
+                  && orderType != (char)CharCodes.ByMin);
 
-            Console.WriteLine("\nSorted rows by max (descending):");
-            sorter.IsLessStrategy = SortOrders.MaxIsLess;
-            sorter.SortMatrix(matrix, ascending: false);
-            PrintMatrix(matrix);
+            switch (orderType)
+            {
+                case (char)CharCodes.BySum:
+                    sorter.IsLessStrategy = SortOrders.SumIsLess;
+                    break;
+                case (char)CharCodes.ByMax:
+                    sorter.IsLessStrategy = SortOrders.MaxIsLess;
+                    break;
+                case (char)CharCodes.ByMin:
+                    sorter.IsLessStrategy = SortOrders.MinIsLess;
+                    break;
+            };
 
-            Console.WriteLine("\nSorted rows by min (ascending):");
-            sorter.IsLessStrategy = SortOrders.MinIsLess;
-            sorter.SortMatrix(matrix);
+            orderType = '0';
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(SORT_TYPE_MSG);
+                orderType = Console.ReadKey(true).KeyChar;
+            } while (orderType != (char)CharCodes.Asc && orderType != (char)CharCodes.Desc);
+
+            sorter.SortMatrix(matrix, ascending: orderType == (char)CharCodes.Asc);
+
+            Console.Clear();
+            Console.WriteLine("Sorted matrix:");
             PrintMatrix(matrix);
         }
     }
